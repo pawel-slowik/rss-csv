feea95f3b2d5abb95d294d715430d564578734ee
Paweł Słowik <pawel.slowik@zoho.com>
2019-09-08 12:46:07 +0200

    initial
    composer create-project koriym/php-skeleton rss-csv

---
+++ tests/RssReaderTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PawelSlowikRekrutacjaHRtec\RssReader;
+
+use PHPUnit\Framework\TestCase;
+
+class RssReaderTest extends TestCase
+{
+    /**
+     * @var RssReader
+     */
+    protected $rssReader;
+
+    protected function setUp() : void
+    {
+        $this->rssReader = new RssReader;
+    }
+
+    public function testIsInstanceOfRssReader() : void
+    {
+        $actual = $this->rssReader;
+        $this->assertInstanceOf(RssReader::class, $actual);
+    }
+}
