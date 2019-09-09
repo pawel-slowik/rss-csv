8e7549107a9f6f03030fb6a572ab59b1d0ab65ee
Paweł Słowik <pawel.slowik@zoho.com>
2019-09-09 10:35:18 +0200

    No need to import the current namespace

--- src/Writer/OverwriteWriter.php
+++ src/Writer/OverwriteWriter.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace PawelSlowikRekrutacjaHRtec\RssReader\Writer;
 
-use PawelSlowikRekrutacjaHRtec\RssReader\Writer\WriterInterface;
 use PawelSlowikRekrutacjaHRtec\RssReader\Exception\RuntimeException;
 
 class OverwriteWriter implements WriterInterface
