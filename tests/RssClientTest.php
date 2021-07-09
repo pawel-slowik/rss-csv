61d378beadb194f121157ea7c4a4aae0dd7d409e
Paweł Słowik <pawel.slowik@zoho.com>
2021-07-09 20:07:46 +0200

    Add test for RssClient

---
+++ tests/RssClientTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RssClient;
+
+use PHPUnit\Framework\TestCase;
+use RssClient\Converter\ConverterInterface;
+use RssClient\Reader\ReaderInterface;
+use RssClient\Writer\WriterInterface;
+
+/**
+ * @covers \RssClient\RssClient
+ */
+class RssClientTest extends TestCase
+{
+    private $rssClient;
+
+    private $reader;
+
+    private $writer;
+
+    private $converter;
+
+    protected function setUp(): void
+    {
+        $this->reader = $this->createMock(ReaderInterface::class);
+        $this->writer = $this->createMock(WriterInterface::class);
+        $this->converter = $this->createMock(ConverterInterface::class);
+
+        $this->rssClient = new RssClient($this->reader, $this->writer, $this->converter);
+    }
+
+    public function testCallsDependencies(): void
+    {
+        $this->reader
+            ->expects($this->once())
+            ->method('fetchIter');
+
+        $this->writer
+            ->expects($this->once())
+            ->method('write');
+
+        $this->converter
+            ->expects($this->atLeastOnce())
+            ->method('convert');
+
+        $this->rssClient->readAndSave('', '');
+    }
+}
