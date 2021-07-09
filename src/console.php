a4d9026e9c4362df22fd342d8ae77cec9ecb38f4
Paweł Słowik <pawel.slowik@zoho.com>
2021-07-09 10:24:46 +0200

    Code formatting fixes

--- src/console.php
+++ src/console.php
@@ -14,16 +14,20 @@ use RssClient\Command\CsvCommand;
 $application = new Application();
 $reader = new Reader();
 
-$application->add(new CsvCommand(
-    new RssClient($reader, new OverwriteWriter()),
-    'csv:simple',
-    'Download a RSS/Atom feed and save its contents into a CSV file.'
-));
+$application->add(
+    new CsvCommand(
+        new RssClient($reader, new OverwriteWriter()),
+        'csv:simple',
+        'Download a RSS/Atom feed and save its contents into a CSV file.'
+    )
+);
 
-$application->add(new CsvCommand(
-    new RssClient($reader, new AppendWriter()),
-    'csv:extended',
-    'Download a RSS/Atom feed and append its contents to a CSV file.'
-));
+$application->add(
+    new CsvCommand(
+        new RssClient($reader, new AppendWriter()),
+        'csv:extended',
+        'Download a RSS/Atom feed and append its contents to a CSV file.'
+    )
+);
 
 $application->run();
